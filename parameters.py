70d54e23c4b4584f9efefe631c1e6af109eaeccd
Tung Nguyen Huy <t.nguyenhuy@tilburguniversity.edu>
2016-04-05 00:47:39 +0200

    A separate file for the parameters are added (no implementation in the notebook files.

---
+++ parameters.py
@@ -0,0 +1,14 @@
+# These are the parameters of the model
+
+T = 100 	# number of periods
+SAFE_R = 1.01 	# return on safe project 1%
+IS_MEAN = 0  	# meean of the idiosyncratic shocks
+IS_STDE = 0.04	# std. error of the distribution of i.s.
+WE_MEAN = 2		# mean of the initial wealth distr.
+WE_STDE = 0.4	# std. dev. of the initial wealth distr.
+BETA = 0.987	# discount preference
+COST_INT = 1.5	# one-time cost of joining the financial int.
+SAMPLING = 0.1  # sampling rate within the fin. intermediary
+
+	# the number of iterations over the  Bellman-equations are in the 
+	# corresponding modules
