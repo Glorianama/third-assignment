14518f41f3edc795116396211f1f9e00096efdf2
Tung Nguyen Huy <t.nguyenhuy@tilburguniversity.edu>
2016-04-05 21:56:00 +0200

    Problems with w_value_function - oneprocess_w.py

--- oneprocess_w.py
+++ oneprocess_w.py
@@ -1,5 +1,4 @@
 import numpy as np
-import multiprocessing
 import scipy as sp
 import scipy.stats
 import parameters as param
@@ -20,10 +19,10 @@ ID_STDEV = param.IS_STDE
 MAX_VAL_E = param.IS_MAXVAL
 MIN_VAL_E = param.IS_MINVAL
 # Number of iterations
-N = param.N2
+N = 1
 COST = param.COST_INT
 
-wealth_axis = np.linspace(1e-6,param.K_MAX,np.ceil(param.K_MAX*0.75))
+wealth_axis = np.linspace(1e-6,param.K_MAX,3) # np.ceil(param.K_MAX*0.75)
 v_func = 76.9230769* log(wealth_axis) + 3569.764136
 
 def aggPDF(x):
@@ -32,32 +31,22 @@ def idiPDF(x):
     return scipy.stats.norm(loc=ID_MEAN,scale=ID_STDEV).pdf(x)
 
 def w_integral(s,W_ax,Y_ax):
-    # max(interp(s*(theta+eps),W_ax,Y_ax),interp(s*(theta+eps)-COST,W_ax,v_func))*aggPDF(theta)*idiPDF(eps)
-    function = lambda theta,eps,s:  theta*eps*s
+    # *aggPDF(theta)*idiPDF(eps)
+    # max(interp(s*(theta+eps),W_ax,Y_ax),interp(s*(theta+eps)-COST,W_ax,v_func))
+    function = lambda theta,eps,s: max(interp(s*(theta+eps),W_ax,Y_ax),interp(s*(theta+eps)-COST,W_ax,v_func))*aggPDF(theta)*idiPDF(eps)
     return dblquad(function,MIN_VAL_E,MAX_VAL_E,lambda x: 0, lambda x: MAX_VAL_AG, args=(s,))
-
-def w_bellman_objective(values, outputArray,l,w_a,w):
-    i = values[0]
-    k = values[1]
-
-    objective = lambda s,k: - log(k-s) - BETA * w_integral(s,wealth_axis,w)[0]
-    s_star = fminbound(objective, 1e-12, k-1e-12, args=(k,))
-    outputArray[i] = -objective(s_star,k)
 
 def w_bellman_op(w):
     Tw = np.empty(wealth_axis.size)
-    Tw_e = multiprocessing.Array('f', Tw)
-    wealth_obj = [[item[0],item[1]] for item in enumerate(wealth_axis)]
-    l = multiprocessing.Lock()
-    workers = [multiprocessing.Process(target=w_bellman_objective, args=(element,Tw_e,
-        l,wealth_axis,w)) for element in enumerate(wealth_obj)]
-    for p in workers:
-        p.start()
-    for p in workers:
-        p.join()
-    return Tw_e
+    for i,k in enumerate(wealth_axis):
+        print "Point ",i+1," of ",len(wealth_axis),"."
+        objective = lambda s,k: - log(k-s) - BETA * w_integral(s,wealth_axis,w)[0]
+        s_star = fminbound(objective, 1e-12, k-1e-12, args=(k,))
+        Tw[i] = -objective(s_star,k)
+    return Tw
 
 w = 10*log(wealth_axis) + 10
 for i in range(N):
+    print i
     w = w_bellman_op(w)
 np.savetxt('w_value_func.csv',w,delimiter=',')
