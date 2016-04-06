f376288e2a1dc797e133eaece5155473bded5b9b
Tung Nguyen Huy <t.nguyenhuy@tilburguniversity.edu>
2016-04-06 12:48:51 +0200

    Debugged main modeule and ran some test. Added some variable paths to be followed.

--- parameters.py
+++ parameters.py
@@ -1,6 +1,6 @@
 # These are the parameters of the model
 
-T = 100 	# number of periods
+T = 30	# number of periods
 SAFE_R = 1.01 	# return on safe project 1%
 IS_MEAN = 0  	# meean of the idiosyncratic shocks
 IS_STDE = 0.04	# std. error of the distribution of i.s.
@@ -8,7 +8,7 @@ IS_MINVAL = -0.5	# Minimum value for the integration
 IS_MAXVAL = 0.5	# Maximum value for the integration
 WE_MEAN = 2		# mean of the initial wealth distr.
 WE_STDE = 0.4	# std. dev. of the initial wealth distr.
-AG_MEAN = 1.02	# aggregate shocks: mean of the distr.
+AG_MEAN = 1.03	# aggregate shocks: mean of the distr.
 AG_STDE = 0.0225	# aggr.shocks: std. dev. of the distr.
 AG_MAXVAL = 2	# Maximum value for the integration
 BETA = 0.987	# discount preference
